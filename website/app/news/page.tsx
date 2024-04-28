dca3a09d5a64c6306242cc3d5baea8b5fcc7d62d
Ellodev <69417638+Ellodev@users.noreply.github.com>
2024-04-28 08:21:06 +0200

    changed data fetching so that it hopefully refreshes the site after 30min

--- website/app/news/page.tsx
+++ website/app/news/page.tsx
@@ -12,6 +12,10 @@ type Post = {
   element_id: number;
 }
 
+type NewsProps = {
+  posts: Post[];
+}
+
 //function to change the dateformat to basically timeAgo
 function timeAgo(dateString: string): string {
   const past = new Date(dateString);
@@ -31,12 +35,24 @@ function timeAgo(dateString: string): string {
 
 
 async function news() {
-  const postsData = await getPosts();
+
+  const posts = await getPosts()
 
   return (
     <div className="flex flex-row flex-wrap min-h-screen items-center justify-center">
-      {postsData.posts ? (
-        postsData.posts.map((post : Post) => (
+      <form>
+        <label>Choose your source:</label>
+        <select id="source" name="source" className="bg-black">
+          <option value="all">All</option>
+          <option value="techcrunch">TechCrunch</option>
+          <option value="wired">Wired</option>
+          <option value="theguardian">The Guardian</option>
+          <option value="bbcnews">BBC News</option>
+        </select>
+      </form>
+
+      {posts ? (
+        posts.map((post: Post) => (
           <div
             key={post.element_id}
             className="m-4 w-96 rounded-md flex flex-col justify-center"
@@ -55,15 +71,17 @@ async function news() {
   );
 }
 
-async function getPosts(): Promise<{ posts: Post[] }> {
-  try {
-    const res = await fetch('https://bucket-tech-news.s3.eu-north-1.amazonaws.com/articles.json');
-    const posts = await res.json();
-    return { posts } // Return an object with the resolved posts array
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return { posts: [] }; // Return an empty array in case of errors
+export async function getPosts() {
+  const res = await fetch('https://bucket-tech-news.s3.eu-north-1.amazonaws.com/articles.json', { next: { revalidate: 1800 } })
+  const posts = await res.json()
+  
+  
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
   }
+
+  return posts
 }
 
 
